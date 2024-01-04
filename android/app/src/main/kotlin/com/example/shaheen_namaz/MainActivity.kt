a2991fe06928b9d0eb76a080ada31ff5f9bc75a1
yaseeen96 <syedyaseen1539@gmail.com>
2024-01-05 00:23:18 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/shaheen_namaz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shaheen_namaz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
