7a2e7a39d9dc0b9660f7bae5ac98013d53e655f4
yaseeen96 <syedyaseen1539@gmail.com>
2024-01-05 23:00:57 +0530

    add delete masjid functionality

--- lib/admin/widgets/masjid_widget.dart
+++ lib/admin/widgets/masjid_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shaheen_namaz/admin/providers/get_masjids_provider.dart';
@@ -11,6 +12,29 @@ class MasjidWidget extends ConsumerWidget {
     return masjids.when(
       data: (data) {
         final masjidList = data.docs;
+        if (masjidList.isEmpty) {
+          return Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "All Masjids",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add),
+                    label: const Text("Add Masjid"),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Text("No Masjids Added. Please Add some"),
+              const Spacer(),
+            ],
+          );
+        }
         return ListView(
           padding: const EdgeInsets.symmetric(horizontal: 10),
           children: [
@@ -39,11 +63,17 @@ class MasjidWidget extends ConsumerWidget {
                       masjidList[index].data()["name"],
                     ),
                     trailing: IconButton(
-                      icon: Icon(
+                      icon: const Icon(
                         Icons.delete,
                         color: Colors.grey,
                       ),
-                      onPressed: () {},
+                      onPressed: () async {
+                        DocumentReference documentReference = FirebaseFirestore
+                            .instance
+                            .collection("Masjid")
+                            .doc(masjidList[index].id);
+                        await documentReference.delete();
+                      },
                     ),
                     tileColor: Theme.of(context).primaryColor,
                     shape: RoundedRectangleBorder(
